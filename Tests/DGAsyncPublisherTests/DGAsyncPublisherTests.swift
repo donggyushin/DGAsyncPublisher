01683452f7c5340ca597b8e960886dac90e934aa
Loopy <loopy@heyratel.com>
2024-09-26 12:00:32 +0900

    asyncMap

---
+++ Tests/DGAsyncPublisherTests/DGAsyncPublisherTests.swift
@@ -0,0 +1,40 @@
+import Testing
+import Foundation
+import Combine
+@testable import DGAsyncPublisher
+
+
+@MainActor
+class ViewModel {
+    @Published var query = ""
+    @Published var result = ""
+    
+    init() {
+        bind()
+    }
+    
+    private func bind() {
+        $query
+            .asyncMap({ await self.asyncFunction(query: $0) })
+            .assign(to: &$result)
+    }
+    
+    private func asyncFunction(query: String) async -> String {
+        return await withCheckedContinuation { continuation in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                continuation.resume(returning: "done")
+            }
+        }
+    }
+}
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    let viewModel = await ViewModel()
+    
+    await #expect(viewModel.result == "")
+    
+    try await Task.sleep(nanoseconds: 2_000_000_000)
+    
+    await #expect(viewModel.result == "done")
+}
